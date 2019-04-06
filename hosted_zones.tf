8ac7549f5b5deea5c66fb6085f76b72e2d089357
Alex Recker <alex@reckerfamily.com>
2019-04-06 00:13:01 -0500

    add terraform

---
+++ hosted_zones.tf
@@ -0,0 +1,33 @@
+resource "aws_route53_zone" "alexrecker_dot_com" {
+  name = "alexrecker.com."
+}
+
+resource "aws_route53_zone" "alexandmarissa_dot_com" {
+  name = "alexandmarissa.com."
+}
+
+resource "aws_route53_zone" "bobrosssearch_dot_com" {
+  name = "bobrosssearch.com."
+}
+
+resource "aws_route53_zone" "sarahrecker_dot_com" {
+  name = "sarahrecker.com."
+}
+
+resource "aws_route53_zone" "astuaryart_dot_com" {
+  name = "astuaryart.com."
+}
+
+resource "aws_route53_zone" "tranquilitydesignsmn_dot_com" {
+  name = "tranquilitydesignsmn.com."
+}
+
+resource "aws_route53_zone" "fromdirktolight_dot_com" {
+  name = "fromdirktolight.com."
+}
+
+resource "aws_route53_zone" "reckerfamily_dot_com" {
+  name = "reckerfamily.com."
+}
+
+
