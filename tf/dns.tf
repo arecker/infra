faf4ff7fb5c3b88e10113767f3f9f268bcbbd454
Alex Recker <alex@reckerfamily.com>
2019-05-29 00:29:30 -0500

    YEAHHHHH

--- tf/dns.tf
+++ tf/dns.tf
@@ -273,24 +273,14 @@ resource "aws_route53_record" "bobrosssearch_dot_com_www" {
   }
 }
 
-resource "aws_route53_record" "bobrosssearch_dot_com_ssl0" {
-  zone_id = "${aws_route53_zone.bobrosssearch_dot_com.zone_id}"
-  name    = "_73a1a2bde3ada19783c8874703f40178.bobrosssearch.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_ecb5d704f17adb5734597fc095a0f621.acm-validations.aws."
-  ]
-}
-
-resource "aws_route53_record" "bobrosssearch_dot_com_ssl1" {
-  zone_id = "${aws_route53_zone.bobrosssearch_dot_com.zone_id}"
-  name    = "_4c1fa76ab065386305f88408b1c5a110.www.bobrosssearch.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_6568253aebf6762a52c4a3aff694cdfa.acm-validations.aws."
-  ]
+module "bobrosssearch_dot_com_cert" {
+  source      = "./modules/cert"
+  zone_name   = "bobrosssearch.com."
+  domain_name = "www.bobrosssearch.com."
+  alts	      = ["bobrosssearch.com."]
+  providers   = {
+    aws = "aws.virginia"
+  }
 }
 
 ####################
@@ -383,27 +373,15 @@ resource "aws_route53_record" "fromdirktolight_dot_com_www" {
   }
 }
 
-resource "aws_route53_record" "fromdirktolight_dot_com_ssl0" {
-  zone_id = "${aws_route53_zone.fromdirktolight_dot_com.zone_id}"
-  name    = "_e06a65824a8d684f9400acb0a4c1aff3.fromdirktolight.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_8b600fdb543c05bf2002a0037ef80f0e.acm-validations.aws."
-  ]
+module "fromdirktolight_dot_com_cert" {
+  source      = "./modules/cert"
+  zone_name   = "fromdirktolight.com."
+  domain_name = "www.fromdirktolight.com."
+  alts	      = ["fromdirktolight.com."]
+  providers   = {
+    aws = "aws.virginia"
+  }
 }
-
-
-resource "aws_route53_record" "fromdirktolight_dot_com_ssl1" {
-  zone_id = "${aws_route53_zone.fromdirktolight_dot_com.zone_id}"
-  name    = "_e979a33ba9a26bb4378b1ed420c9b574.www.fromdirktolight.com"
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_b354d4481e1216fd9cf6a283a4d5d8ef.acm-validations.aws."
-  ]
-}
-
 
 ############################
 # tranquilitydesignsmn.com #
@@ -441,34 +419,14 @@ module "tranquilitydesignsmn_dot_com_privateemail" {
   zone_name = "tranquilitydesignsmn.com."
 }
 
-resource "aws_route53_record" "tranquilitydesignsmn_dot_com_ssl0" {
-  zone_id = "${aws_route53_zone.tranquilitydesignsmn_dot_com.zone_id}"
-  name    = "_558238ef010d11dbe632d392943988d5.tranquilitydesignsmn.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_7316d0699714bf38571ab2e0610cd2f8.acm-validations.aws.",
-  ]
-}
-
-resource "aws_route53_record" "tranquilitydesignsmn_dot_com_ssl1" {
-  zone_id = "${aws_route53_zone.tranquilitydesignsmn_dot_com.zone_id}"
-  name    = "_e1bf33ddf9b6f9d90fd6812be7437d9e.www.tranquilitydesignsmn.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_66f1c0e425905e885823c1bfa6b38783.acm-validations.aws.",
-  ]
-}
-
-resource "aws_route53_record" "tranquilitydesignsmn_dot_com_ssl2" {
-  zone_id = "${aws_route53_zone.tranquilitydesignsmn_dot_com.zone_id}"
-  name    = "_5bfba514fa57efad86cc0f606c890464.api.tranquilitydesignsmn.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_e8fb87937c1cf4f5a6fa7dd3097368c3.acm-validations.aws.",
-  ]
+module "tranquilitydesignsmn_dot_com_cert" {
+  source      = "./modules/cert"
+  zone_name   = "tranquilitydesignsmn.com."
+  domain_name = "tranquilitydesignsmn.com."
+  alts	      = ["www.tranquilitydesignsmn.com", "api.tranquilitydesignsmn.com"]
+  providers   = {
+    aws = "aws.virginia"
+  }
 }
 
 resource "aws_route53_record" "tranquilitydesignsmn_dot_com_www" {
@@ -587,6 +545,34 @@ resource "aws_route53_record" "sarahrecker_dot_com_blog" {
   }
 }
 
+module "sarahrecker_dot_com_cert" {
+  source      = "./modules/cert"
+  zone_name   = "sarahrecker.com."
+  domain_name = "www.sarahrecker.com."
+  alts	      = ["sarahrecker.com."]
+  providers   = {
+    aws = "aws.virginia"
+  }
+}
+
+module "sarahrecker_dot_com_blog_cert" {
+  source      = "./modules/cert"
+  zone_name   = "sarahrecker.com."
+  domain_name = "blog.sarahrecker.com."
+  providers   = {
+    aws = "aws.virginia"
+  }
+}
+
+module "sarahrecker_dot_com_api_cert" {
+  source      = "./modules/cert"
+  zone_name   = "sarahrecker.com."
+  domain_name = "api.sarahrecker.com."
+  providers   = {
+    aws = "aws.virginia"
+  }
+}
+
 resource "aws_route53_record" "sarahrecker_dot_com_api" {
   zone_id = "${aws_route53_zone.sarahrecker_dot_com.zone_id}"
   name    = "api.sarahrecker.com"
@@ -597,54 +583,4 @@ resource "aws_route53_record" "sarahrecker_dot_com_api" {
     zone_id		   = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
-}
-
-resource "aws_route53_record" "sarahrecker_dot_com_ssl0" {
-  zone_id = "${aws_route53_zone.sarahrecker_dot_com.zone_id}"
-  name    = "_a904d2f1c37e9ed523909903104e2f4f.sarahrecker.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_c55fcb6d34cbd7fae0fd2dd3ebe57349.acm-validations.aws."
-  ]
-}
-
-resource "aws_route53_record" "sarahrecker_dot_com_ssl1" {
-  zone_id = "${aws_route53_zone.sarahrecker_dot_com.zone_id}"
-  name    = "_65aa3f939c04fc0ba16c47b7f32380bc.api.sarahrecker.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_4c3d9f0d2edd74694cdcc7c3026ea0c8.acm-validations.aws."
-  ]
-}
-
-resource "aws_route53_record" "sarahrecker_dot_com_ssl2" {
-  zone_id = "${aws_route53_zone.sarahrecker_dot_com.zone_id}"
-  name    = "_8a25dcb0990177b5dc4abc65c00d4aad.blog.sarahrecker.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_0d9318ed6bee8625c5ccf4fc53a77e87.acm-validations.aws."
-  ]
-}
-
-resource "aws_route53_record" "sarahrecker_dot_com_ssl3" {
-  zone_id = "${aws_route53_zone.sarahrecker_dot_com.zone_id}"
-  name    = "_ef5a516a3c3d85fbef582251d326ab25.scrabble.sarahrecker.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_347230f0b7e6715ad4114a103a51ef5b.acm-validations.aws."
-  ]
-}
-
-resource "aws_route53_record" "sarahrecker_dot_com_ssl4" {
-  zone_id = "${aws_route53_zone.sarahrecker_dot_com.zone_id}"
-  name    = "_85dbd81e1ba1f2f4787472ded24d2579.www.sarahrecker.com."
-  type    = "CNAME"
-  ttl     = "60"
-  records = [
-    "_e971b76034430720efb59876fa9dffb7.acm-validations.aws."
-  ]
 }
