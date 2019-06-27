e5f9df478695b2426682581fee94422cf3a47309
Alex Recker <alex@reckerfamily.com>
2019-06-27 07:33:41 -0500

    oops

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -7,5 +7,5 @@ resource "aws_s3_bucket_object" "contact" {
   bucket = "${aws_s3_bucket.lambda_bucket.id}"
   key    = "contact.py"
   source = "lambdas/contact.py"
-  etag	 = "${filemd5("path/to/file")}"
+  etag	 = "${filemd5("lambdas/contact.py")}"
 }
