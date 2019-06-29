42fe221775c821b01479880709d6ff14798c3b9d
Alex Recker <alex@reckerfamily.com>
2019-06-28 21:58:14 -0500

    ooh yeah

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -6,3 +6,12 @@ resource "aws_iam_user_policy_attachment" "public_travis_attachment" {
   user       = "${aws_iam_user.public_travis.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+resource "aws_iam_user" "infra_travis" {
+  name = "infra-travis-service"
+}
+
+resource "aws_iam_user_policy_attachment" "infra_travis_attachment" {
+  user       = "${aws_iam_user.infra_travis.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
