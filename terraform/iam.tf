8596cd7a063341dfb716d13de1804a3515642879
Alex Recker <alex@reckerfamily.com>
2019-06-30 22:15:31 -0500

    cache invalidation

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -15,3 +15,26 @@ resource "aws_iam_user_policy_attachment" "infra_travis_attachment" {
   user       = "${aws_iam_user.infra_travis.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "cache-invalidate-policy"
+  user = "${aws_iam_user.public_travis.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "*",
+      "Action": [
+        "cloudfront:CreateInvalidation",
+        "cloudfront:GetDistribution",
+        "cloudfront:ListDistributions",
+        "cloudfront:ListTagsForResource"
+      ]
+    }
+  ]
+}
+EOF
+}
