fa2ba88a3d0ae5acb60ac95c2de61ee69a01b196
Alex Recker <alex@reckerfamily.com>
2019-09-09 12:04:52 -0500

    Fix

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ case "$1" in
 	TF_VERSION="$(cat ./terraform/.terraform-version)"
 	log "using terraform version $TF_VERSION"
 	URL_BASE="https://releases.hashicorp.com"
-	URL="$URL_BASE/terraform/${TF_VERSION}/terraform_{$TF_VERSION}_linux_amd64.zip"
+	URL="$URL_BASE/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip"
 	log "downloading $URL"
 	curl -sLo /tmp/terraform.zip "$URL"
 	log "unpacking $HOME/.local/bin/terraform"
