8ebda4dcd7dacf85ddd4a7b8f3a701f587327574
Alex Recker <alex@reckerfamily.com>
2020-02-25 10:14:54 -0600

    STUFF

--- terraform/aws/ddns.tf
+++ terraform/aws/ddns.tf
@@ -1,23 +1,23 @@
-resource "aws_iam_user" "ddns" {
-  name = "ddns-role"
-}
+# resource "aws_iam_user" "ddns" {
+#   name = "ddns-role"
+# }
 
-resource "aws_iam_user_policy" "ddns" {
-  name = "ddns-route53-policy"
-  user = "${aws_iam_user.ddns.name}"
+# resource "aws_iam_user_policy" "ddns" {
+#   name = "ddns-route53-policy"
+#   user = "${aws_iam_user.ddns.name}"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "route53:ChangeResourceRecordSets"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "route53:ChangeResourceRecordSets"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
