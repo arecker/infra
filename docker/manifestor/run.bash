8b080e2bcb44bfe9abe5b97388f743c7dfd072eb
Alex Recker <alex@reckerfamily.com>
2020-10-18 14:39:35 -0500

    This all sucks

--- docker/manifestor/run.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-log() {
-    echo "manifestor: $1" 1>&2;
-}
-
-log "using kubectl $(kubectl version --short=true | head -1 | awk '{ print $3 }')"
-
-kubectl apply -f "$MANIFEST_URL"
