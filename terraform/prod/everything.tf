02b82dab59595b06c4a8daf2d63bd0c7a6ff83b0
Alex Recker <alex@reckerfamily.com>
2021-04-07 11:02:26 -0500

    Move cookbook to cloudflare

--- terraform/prod/everything.tf
+++ terraform/prod/everything.tf
@@ -28,24 +28,6 @@ data "digitalocean_droplet" "prod" {
   name = "prod"
 }
 
-resource "digitalocean_domain" "cookbook" {
-  name = "thereckerfamilycookbook.com"
-}
-
-resource "digitalocean_record" "cookbook_www" {
-  domain = digitalocean_domain.cookbook.name
-  type   = "A"
-  name   = "www"
-  value  = data.digitalocean_droplet.prod.ipv4_address
-}
-
-resource "digitalocean_record" "cookbook_apex" {
-  domain = digitalocean_domain.cookbook.name
-  type   = "A"
-  name   = "@"
-  value  = data.digitalocean_droplet.prod.ipv4_address
-}
-
 resource "cloudflare_zone" "cookbook" {
     zone = "thereckerfamilycookbook.com"
 }
