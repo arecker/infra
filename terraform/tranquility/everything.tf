f1d52e6add129aa0feb332a1d7b5afb6098e500d
Alex Recker <alex@reckerfamily.com>
2021-04-08 00:23:46 -0500

    Base projects

---
+++ terraform/tranquility/everything.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "recker-terraform"
+    key    = "tranquility.tfstate"
+    region = "us-east-2"
+  }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  email   = "alex@reckerfamily.com"
+  api_key = file("${path.module}/../secrets/cloudflare")
+}
+
+resource "cloudflare_zone" "zone" {
+  zone = "tranquilitydesignsmn.com"
+}
